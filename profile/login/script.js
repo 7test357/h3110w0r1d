28ac19062388c386193cc6bcdd803785e9cecc4f
7test357 <7test357@gmail.com>
2024-12-01 19:28:39 +0900

    Update script.js

--- profile/login/script.js
+++ profile/login/script.js
@@ -3,10 +3,8 @@ document.getElementById('login-button').addEventListener('click', function() {
     const password = document.getElementById('password').value;
     const messageElement = document.getElementById('message');
 
-    // 이메일 정규 표현식
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // 이메일 유효성 검사
     if (!emailPattern.test(email)) {
         messageElement.textContent = '유효한 이메일 주소를 입력하세요.';
         messageElement.style.color = '#ff0000'; // 오류 메시지 색상
@@ -26,18 +24,4 @@ document.getElementById('login-button').addEventListener('click', function() {
         },
         body: JSON.stringify(data)
     })
-    .then(response => {
-        if (response.ok) {
-            messageElement.textContent = '로그인 정보가 전송되었습니다.';
-            messageElement.style.color = '#00ff00'; // 성공 메시지 색상
-        } else {
-            messageElement.textContent = '전송 중 오류가 발생했습니다.';
-            messageElement.style.color = '#ff0000'; // 오류 메시지 색상
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        messageElement.textContent = '전송 중 오류가 발생했습니다.';
-        messageElement.style.color = '#ff0000'; // 오류 메시지 색상
-    });
 });
